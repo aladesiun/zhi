87ac8b8eca1e111e75b978c6c6c4dd7e94b61084
temitope <83991100+aladesiun@users.noreply.github.com>
2022-03-08 11:04:49 +1300

    ff

---
+++ src/components/nav.vue
@@ -0,0 +1,47 @@
+<template>
+        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-1 rounded dark:bg-gray-800 ">
+    <div class="container flex flex-wrap justify-between items-center mx-auto">
+        <a href="#" class="flex items-center">
+            <span class="self-center text-5xl font-semibold whitespace-nowrap dark:text-white">ZHIVAGO</span>
+        </a>
+        <div class="flex md:order-2">
+            <router-link
+                to="/login"
+                class="text-black border bg-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2  mx-1 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+                Get started
+            </router-link>
+             <router-link
+                to="/signup"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2  ml-3 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+                Get started
+            </router-link>
+            <button
+                data-collapse-toggle="mobile-menu-4"
+                type="button"
+                class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="mobile-menu-4"
+                aria-expanded="false"
+            >
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                </svg>
+                <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        fill-rule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                    ></path>
+                </svg>
+            </button>
+        </div>
+        <div class="hidden justify-between items-center  md:flex md:w-auto md:order-1" id="mobile-menu-4">
+          <div class="relative">
+            <input type="text" placeholder="search" id="base-input" class="bg-searchinputcolor  border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-[600px] p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"> 
+          </div>
+        </div>
+    </div>
+</nav>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import login from '../views/login.vue'
 import signup from '../views/signup.vue'
+import home from '../views/Home'
 
 const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: home
+  },
   {
     path: '/login',
     name: 'login',
