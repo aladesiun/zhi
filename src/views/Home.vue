9297f1459b1542eeafe323129b519b43771f6d0d
Kennypapa <81877552+Kennypapa@users.noreply.github.com>
2022-03-08 08:22:11 -0800

    ll

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home bg-homebg h-[66px]">
+    <Navheader/>
+    <div class="home-content">
+      hello
+    </div>
+  </div>  
+</template>
+
+<script>
+import Navheader from "../components/nav.vue";
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    Navheader
+}
+}
+</script>
